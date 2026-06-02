c674037d6e27d3afd55fab64305d9a2f973be89f
tanisharoy222005-collab <tanisharoy222005@gmail.com>
2026-06-02 23:48:03 +0530

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -1,20 +1,48 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import ProductCard from "../components/ProductCard";
 import products from "../data/products";
 
 export default function Products() {
+  const [search, setSearch] =
+    useState("");
+
+  const filtered =
+    products.filter((product) =>
+      product.name
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+
   return (
     <Layout>
-      <h1>Product Catalog</h1>
+
+      <h1
+        style={{
+          marginBottom: "20px"
+        }}
+      >
+        Product Catalog
+      </h1>
+
+      <input
+        className="search-box"
+        placeholder="Search products..."
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+      />
 
       <div className="grid">
-        {products.map((product) => (
+        {filtered.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
           />
         ))}
       </div>
+
     </Layout>
   );
 }
